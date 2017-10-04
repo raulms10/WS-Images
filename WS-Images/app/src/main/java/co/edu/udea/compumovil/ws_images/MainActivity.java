a42b5df21b75f04c7fb7809a2590aa55b787aaae
raulms10 <rantonio.martinez@udea.edu.co>
2017-10-04 14:45:50 -0500

    Cambiando HOST

--- WS-Images/app/src/main/java/co/edu/udea/compumovil/ws_images/MainActivity.java
+++ WS-Images/app/src/main/java/co/edu/udea/compumovil/ws_images/MainActivity.java
@@ -62,9 +62,9 @@ public class MainActivity extends AppCompatActivity {
     private final String URL_CONTAINER_UP_COMPLEMENTO = ":3000/api/Containers/all/upload";
 
     /* Si se trabaja local se quita comentario de estas lineas y se comenta las de CodeAnyWhere
-    private final String URL_STUDENTS = HOST_CODE.concat(URL_STUDENTS_COMPLEMENTO);
-    private final String URL_CONTAINER_DOWN = HOST_CODE.concat(URL_CONTAINER_DOWN_COMPLEMENTO);
-    private final String URL_CONTAINER_UP = HOST_CODE.concat(URL_CONTAINER_UP_COMPLEMENTO);
+    private final String URL_STUDENTS = HOST_IP.concat(URL_STUDENTS_COMPLEMENTO);
+    private final String URL_CONTAINER_DOWN = HOST_IP.concat(URL_CONTAINER_DOWN_COMPLEMENTO);
+    private final String URL_CONTAINER_UP = HOST_IP.concat(URL_CONTAINER_UP_COMPLEMENTO);
     */
     /* Si se trabaja con CondeAnyWhere*/
     private final String URL_STUDENTS = HOST_CODE.concat(URL_STUDENTS_COMPLEMENTO);
